f7ae38bb1a58006a5717322dc36103940b65996a
Pramuditha Karunarathna <shemil076@gmail.com>
2025-04-16 15:14:07 +0530

    :sparkles: Implemented the check auth middleware

--- internal/services/user_service.go
+++ internal/services/user_service.go
@@ -1,3 +1,27 @@
 package services
 
+import (
+	"database/sql"
+	"fmt"
 
+	"github.com/shemil076/loyalty-backend/internal/database"
+	"github.com/shemil076/loyalty-backend/internal/models"
+)
+
+
+func GetUserById(userId string) (models.User, error) {
+
+	var user models.User
+	query := `
+		SELECT id, phoneNumber FROM users WHERE id = ?
+	`
+	err := database.DB.QueryRow(query, userId).Scan(&user.ID, &user.PhoneNumber)
+
+	if (err == sql.ErrNoRows){
+		return user,  fmt.Errorf("user not found")
+	}else if (err != nil){
+		return user, fmt.Errorf("failed to query user: %v", err)
+	}
+
+	return user, nil
+}
